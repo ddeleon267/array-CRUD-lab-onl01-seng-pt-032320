010d2015a905e31bee9d04431d8e0c5c928e294e
Dominique De León <dominique.a.deleon@gmail.com>
2020-06-25 16:47:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,3 +1,5 @@
+
+
 def create_an_empty_array
   
 end
@@ -7,7 +9,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
+  binding.pry
 end
 
 def add_element_to_start_of_array(array, element)
--- spec/array_crud_spec.rb
+++ spec/array_crud_spec.rb
@@ -1,17 +1,17 @@
 require "spec_helper"
 
-describe "#create_an_empty_array" do
-  it "creates and returns and empty array" do 
-    expect(create_an_empty_array).to eq([])
-  end 
-end
+# describe "#create_an_empty_array" do
+#   it "creates and returns and empty array" do 
+#     expect(create_an_empty_array).to eq([])
+#   end 
+# end
 
-describe "create_an_array" do 
-  it "creates an array with four elements" do 
-    expect(create_an_array).to be_a(Array)
-    expect(create_an_array.size).to eq(4)
-  end
-end
+# describe "create_an_array" do 
+#   it "creates an array with four elements" do 
+#     expect(create_an_array).to be_a(Array)
+#     expect(create_an_array.size).to eq(4)
+#   end
+# end
 
 describe "add_element_to_end_of_array" do 
   it "takes arguments of an array and an element and adds that element to the end of the array" do 
